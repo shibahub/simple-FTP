522ad79498ddf438d7bdda0c64e433b4599518ad
shiba <shiba@shibas-MacBook-Pro.local>
2022-09-13 15:46:45 +0700

    test

--- server/server.py
+++ server/server.py
@@ -1,12 +1,12 @@
 from audioop import add
 from http import client
 import socket
-
+from subprocess import call
 from setuptools import Command
 
 socket_server= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-host= '127.0.0.1'
+host= ''
 port= 9090
 
 def dowload():
@@ -19,23 +19,35 @@ greeting='\033[1;32;40m ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠒⠊�
 msg='Welcome to FTP server'
 socket_server.bind((host,port))
 socket_server.listen(5)
+
 while True:
     print(greeting + '\n\nWelcome to FTP SERVER')
     print('waiting for the connection ............')
     #print(socket_server.accept())
     conn, addr = socket_server.accept()
     #print(conn)
-    print(f'Reieve connection from: {addr}')
-    msg= msg+f' USER:{addr}\nWant to download press (d), upload press (u):'
-    conn.sendall(msg.encode('utf-8'))
-    comm = conn.recv(1024)
-    comm = comm.decode('utf-8')
-    if comm=='d':
-        dowload()
-    if comm=='u':
-        upload()
-    #print(comm.decode('utf-8'))
-    #socket_server.send(msg.encode('utf-8'))
-    print('shiba')
-    break
+    try:
+        print(f'Reieve connection from: {addr}')
+        msg= msg+f' USER:{addr}\nWant to download press (d), upload press (u):, list press(ls)'
+        conn.sendall(msg.encode('utf-8'))
+        comm = conn.recv(1024)
+        comm = comm.decode('utf-8')
+        
+        if comm=='d':
+            dowload()
+        if comm=='u':
+            upload()
+        if comm=='ls':
+            rc=call("./test.sh", shell=True)
+            print(type(rc))
+            socket_server.send(rc)
+        #print(comm.decode('utf-8'))
+        #socket_server.send(msg.encode('utf-8'))
+        #print('shiba')
+        break
+
+    except:
+        print('closing socket Error occur')
+        print ('')
+        break
 conn.close()
