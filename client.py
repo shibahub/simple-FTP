7a32e6b79b72bf6554cbd3d0b4bb06d7f88042a9
shiba <shiba@shibas-MacBook-Pro.local>
2022-09-13 13:59:06 +0700

    v.2

--- client.py
+++ client.py
@@ -5,21 +5,13 @@ port = 9090
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 clientSocket.connect((host,port))
-print(f'\033[1;31;40mConnecting to {host}:{port}......')
-data = "Hello Server!"
-
-clientSocket.send(data.encode())
-
- 
-
-# Receive data from server
-
+print(f'\033[1;32;40mConnecting to {host}:{port}......')
 dataFromServer = clientSocket.recv(1024)
-
- 
-
-# Print to the console
-
 print(dataFromServer.decode('utf-8'))
+data=input('')
+clientSocket.send(data.encode('utf-8'))
+
+
+
 
 clientSocket.close()
