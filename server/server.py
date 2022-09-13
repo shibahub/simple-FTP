a797ffba89721c1fdf423ad5eef269ec480362dd
shiba <shiba@shibas-MacBook-Pro.local>
2022-09-13 23:43:08 +0700

    server v3.py

--- server/server.py
+++ server/server.py
@@ -1,53 +1,130 @@
-from audioop import add
-from http import client
+from base64 import encode
+from distutils.log import error
 import socket
 from subprocess import call
-from setuptools import Command
+import struct
+import sys
+import os
+import time
+import json
 
 socket_server= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 host= ''
 port= 9090
 
-def dowload():
-    print('shiba')
+size= 1024
+code='utf-8'
 
-def upload():
-    print('innue')
+def dowload(conn):
+    print('User request for download')
+    file=conn.recv(1024)
+    filename=file.decode(code)
+    print ('Filename :',filename)
+    if os.path.isfile(filename):
+        f = open(filename,'rb')
+        l = f.read()
+        #print(l)
+        while (l):
+        #
+            conn.send(l)
+            print('Sent ',repr(l))
+            #l = f.read(1024)
+            #print('shiba')
+            break
+        print('Done sending')
+            #conn.send(msg.encode(code))
+        #conn.send(struct.pack("i", os.path.getsize(filename)))
+    else:
+        print('file not found')
+        msg='File not found'
+        conn.send(msg.encode(code))
+   
+    return
+
+def upload(conn):
+    try:
+        f_name = json.loads(f_name.decode())
+        print(f'file name: {f_name.get("file_name")}')
+        #receive file data from client
+        data = conn.recv(2048)
+        #create file and write data
+        f = open(f_name.get("file_name"), 'wb')
+        f.write(data)
+        conn.close()
+        conn.close()
+        f.close()
+    except:
+        print("[ERROR]")
+        conn.close()
+    return
+
+def listfile(conn):
+    print ("Listing directory")
+    listing = os.listdir(os.getcwd())
+    print(listing)
+    leng =str(len(listing))
+    conn.send(leng.encode(code))
+    for i in listing:
+        #print(len(i))
+        #print(sys.getsizeof(i))
+        lenI=str(len(i))
+        conn.sendall(lenI.encode(code))
+        #conn.request.sendall(encode(code))
+        #print('shiba')
+        #print(lenI)
+        conn.send(i.encode(code))
+        #print('shiba')
+        Fsize= sys.getsizeof(i)
+        Fsize=str(Fsize)
+        conn.send(Fsize.encode(code))
+        
+    return
+
 
 greeting='\033[1;32;40m ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠒⠊⠉⠉⠉⠒⠲⢤⣀⠀⠀⠀⠀⠀⣀⣤⠤⠶⠒⠶⠤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠦⡤⠖⠋⠁⠀⠀⠀⠀⠀⠀⠀⠙⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⢀⣀⣠⠤⢤⣀⣀⡀⠀⠀⠹⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠀⠀⠀⠀⣠⠴⠚⠉⠀⠀⠀⠀⠀⠀⠉⠙⠲⢤⣹⣀⣀⡤⠤⠤⠤⠤⠤⠤⢄⣀⣈⣇⡀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢀⡞⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣙⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠓⢦⡀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⣀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠒⣊⡭⠥⠔⠒⠲⠦⠤⢭⣉⣳⣄⣤⣴⣒⣊⡭⠭⠭⠭⠭⠭⣿⣶⣻⣦⣀⠀\n⠀⠀⠀⢀⡴⠚⢹⠃⠀⠀⠀⠀⠀⠀⢀⡤⠖⢚⣡⠖⠋⠁⠀⠀⠀⠀⠀⢀⣀⣀⣀⣙⣿⡛⠉⠁⠀⢀⣀⣀⣠⣤⣤⣤⠤⣭⣝⣿⣄\n⠀⠀⢠⡞⠁⠀⣾⠀⠀⠀⠀⠀⠀⣾⣛⣛⠋⠉⢀⣀⣀⡠⠤⢶⣶⢿⣿⣿⣤⡀⠀⠀⠈⡷⠒⠚⠉⠉⢠⣿⡿⢿⣿⣿⣦⡀⠀⠉⢻\n⠀⢀⡏⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠯⣉⠀⠀⠀⢠⣿⣿⣶⣿⠛⢻⣿⡆⠀⣰⠁⠀⠀⠀⠀⣿⣿⠿⣿⣏⣹⣿⣧⢀⣠⡞\n⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠦⢬⣙⠒⠤⢼⠿⢿⡿⠿⠿⠿⠛⠛⢉⡼⠛⠓⠒⠒⠶⠟⠛⠛⠛⠛⠛⠋⢩⡿⠛⠀\n⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠒⠒⠒⠒⠒⠒⣲⡾⠉⠉⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠋⠀⠀⠀\n⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠶⠋⠁⠀⠀⠀⠀⠈⠛⠢⢤⣤⠤⠤⠴⠒⢿⡁⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠙⢦⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣹⣄⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠋⠁⡀⠀⣀⡀⠀⠉⠉⠙⠓⠒⠲⠦⠤⠤⣤⣀⣀⣀⣀⣀⣀⣀⣀⣀⣤⠤⠶⠚⠉⢉⣿⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡅⠀⠀⠉⠉⠉⠉⠉⠓⠒⠶⠤⢤⣤⣀⣀⣀⣀⡀⠀⠀⠉⠉⠉⠉⠁⣀⣀⣀⣀⣠⣴⠟⠁⠀⠀\n⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀⠙⠒⠒⠒⠒⠒⠲⠦⠤⠤⣀⣀⣀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⢀⣿⠀⠀⠀⠀\n⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠦⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠙⠛⠛⠒⠒⠒⠒⠶⠶⠶⠶⢶⡦⠶⠒⠋⠁⠀⠀⠀⠀\n⠟⠿⢿⡶⢤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠔⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠉⠓⠦⣭⣉⠓⠒⠶⠦⠤⢤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⡤⠖⠚⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠈⠙⠓⠲⠦⢤⣤⣤⣀⣀⣀⣉⣉⣉⣉⣉⡉⢉⣉⣉⣉⣉⣩⣭⠟⠛⠷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠈⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'
 msg='Welcome to FTP server'
 socket_server.bind((host,port))
 socket_server.listen(5)
 
-while True:
-    print(greeting + '\n\nWelcome to FTP SERVER')
-    print('waiting for the connection ............')
-    #print(socket_server.accept())
-    conn, addr = socket_server.accept()
-    #print(conn)
-    try:
-        print(f'Reieve connection from: {addr}')
-        msg= msg+f' USER:{addr}\nWant to download press (d), upload press (u):, list press(ls)'
-        conn.sendall(msg.encode('utf-8'))
-        comm = conn.recv(1024)
-        comm = comm.decode('utf-8')
-        
-        if comm=='d':
-            dowload()
-        if comm=='u':
-            upload()
-        if comm=='ls':
-            rc=call("./test.sh", shell=True)
-            print(type(rc))
-            socket_server.send(b'f{rc}')
-        #print(comm.decode('utf-8'))
-        #socket_server.send(msg.encode('utf-8'))
-        #print('shiba')
-        break
 
-    except:
-        print('closing socket Error occur')
-        print ('')
-        break
+print(greeting + '\n\nWelcome to FTP SERVER')
+print('waiting for the connection ............')
+conn, addr = socket_server.accept() 
+    #print(conn)
+try:
+        while True:
+            print(f'Reieve connection from: {addr}')
+            
+            
+            msg= msg+f' USER:{addr}\nWant to download press (d), upload press (u):, show list press(ls), exit (q)'
+            conn.sendall(msg.encode('utf-8'))
+            comm = conn.recv(size)
+            comm = comm.decode(code)
+        
+            if comm=='d':
+                
+                dowload(conn)
+            if comm=='u':
+                upload(conn)
+            if comm=='ls':
+                listfile(conn)
+                #string=''
+                #rc=call("./test.sh", shell=True)
+                #string=string+call("./test.sh", shell=True)
+                #print('shiba')
+                #print(type(string))
+                #conn.send('<=========== LIST OF FILE =============>\n'.encode(code))
+                #conn.send(string.encode(code))
+            if comm=='q':
+                conn.close()
+                break
+            #print(comm.decode('utf-8'))
+            #socket_server.send(msg.encode('utf-8'))
+            #print('shiba')
+                
+
+except:
+    print('closing socket Error occur')
+        #print (error)
 conn.close()
