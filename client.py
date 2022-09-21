3a74c2f2382dd526024aafd177d5c3cfbf26d793
shibahub <78590473+shibahub@users.noreply.github.com>
2022-09-21 13:38:48 +0700

    shiva

--- client.py
+++ client.py
@@ -3,126 +3,139 @@ import struct
 import os
 import sys
 import json
-from numpy import spacing
+
+from numpy import size, spacing
+
 
 host = '127.0.0.1'
 port = 9090
-
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def listfile():
     try:
-
-        print('Index  File               Size')
-        leng = clientSocket.recv(1)
-        leng = leng.decode('utf-8')
-        leng = int(leng)
-        print(leng)
-        for i in range(leng):
-            # print(i)
-            # data=clientSocket.req
+        i='Index'
+        f='File'
+        s='Size'
+        print('-'*40)
+        print('{:<6s} {:<16s} {:>9s}'.format(i,f,s))
+        print('-'*40)
+        #print('Index  File                  Size')
+        #leng=clientSocket.recv(1)
+        #leng=leng.decode('utf-8')
+        #leng=int(leng)
+        #print(leng)
+        data= clientSocket.recv(1024)
+        data=data.decode('utf-8')
+        print(data)
+        """for i in range(leng):
+            #print(i)
+            #data=clientSocket.req
             data = clientSocket.recv(1)
-            # print(data)
-            lenI = data.decode('utf-8')
-            lenI = int(lenI)
-            # print(lenI)
-            data = clientSocket.recv(lenI)
-            data = data.decode('utf-8')
-            size = clientSocket.recv(2)
-            size = size.decode('utf-8')
-            print(f'{i}.    {data}                {size}')
+            #print(data)
+            lenI=data.decode('utf-8')
+            lenI=int(lenI)
+            #print(lenI)
+            data=clientSocket.recv(lenI)
+            data=data.decode('utf-8')
+            size=clientSocket.recv(2)
+            size=size.decode('utf-8')
+            print(f'{i}.    {data}                {size}')"""
     except:
-        print("Could not found the file, some error occur")
+        print ("Could not found the file, some error occur")
     return
-
 
 def dowload():
     try:
-        inp = input('>Please INput file name: ')
+        inp= input('> Input file name (download): ')
         clientSocket.send(inp.encode('utf-8'))
-        res = clientSocket.recv(14)  # problem with size
-        print(res.decode('utf-8'))
-        with open(inp+'2', 'wb') as f:
-            print('file opened')
-            while True:
-                print('receiving data...')
-                data = clientSocket.recv(1024)
-                print(data)
-                # print('shiba')
-                # print('data=', (data))
-                f.write(data)
-                if not data:
+        #res=clientSocket.recv(14) # problem with size
+        #print(res.decode('utf-8'))
+        res=clientSocket.recv(1)
+        res.decode('utf-8')
+        if res =='1':
+            with open('down-'+inp, 'wb') as f:
+                #print ('file opened')
+                while True:
+                    print('receiving data...')
+                    data = clientSocket.recv(2048)
+                    for i in range(0,25):
+                        print('=',end='')
+                        if i==24:
+                            print('> 100%')
+
+                    #print(data)
+                    #print('shiba')
+                    #print('data=', (data))
+                    f.write(data)
+                    if not data:
+                        break
                     break
-                break
-
+            print('File name: "down-'+inp+'" download sucess')
+        else:
+            print('File not found')
+            
     except:
-        print("Could not found the file, some error occur")
+        print ("Could not found the file, some error occur")
     return
-
-
+    
 def upload():
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    while True:
-        file_name = input(">Enter file name: ")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        file_name = input("> Enter file name: ")
         try:
-            # If file exceed 2MB according to ajarn
+            #If file exceed 2MB according to ajarn
             if os.path.getsize(file_name) < 2097152:
-                # กุส่งเป็น json ทำไมวะ ขกแก้ละ LOL
+                
                 dt = {
-                    "file_name": file_name
+                    "file_name": file_name 
                 }
                 dts = json.dumps(dt)
-                clientSocket.send(dts.encode())
-                # Send file's data
+                s.send(dts.encode())
+                #Send file's data
                 fname = file_name
                 f = open(fname, 'rb')
                 l = f.read(2048)
-                while (l):
-                    clientSocket.send(l)
+                while (l) :
+                    s.send(l)
                     l = f.read(2048)
                 f.close()
-                # clientSocket.close()
-            else:
+                s.close()
+            else :
                 print('FILE SIZE EXCEED 2MB')
         except:
-            # clientSocket.close()
-            print("FILE NAME DOESN'T EXIST")
-            break
-        break
+            s.close()
+            print("no such file")    
 
 
-try:
-    clientSocket.connect((host, port))
+try: 
+    greeting='\033[1;32;40m ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠒⠊⠉⠉⠉⠒⠲⢤⣀⠀⠀⠀⠀⠀⣀⣤⠤⠶⠒⠶⠤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠦⡤⠖⠋⠁⠀⠀⠀⠀⠀⠀⠀⠙⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⢀⣀⣠⠤⢤⣀⣀⡀⠀⠀⠹⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠀⠀⠀⠀⣠⠴⠚⠉⠀⠀⠀⠀⠀⠀⠉⠙⠲⢤⣹⣀⣀⡤⠤⠤⠤⠤⠤⠤⢄⣀⣈⣇⡀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢀⡞⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣙⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠓⢦⡀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⣀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠒⣊⡭⠥⠔⠒⠲⠦⠤⢭⣉⣳⣄⣤⣴⣒⣊⡭⠭⠭⠭⠭⠭⣿⣶⣻⣦⣀⠀\n⠀⠀⠀⢀⡴⠚⢹⠃⠀⠀⠀⠀⠀⠀⢀⡤⠖⢚⣡⠖⠋⠁⠀⠀⠀⠀⠀⢀⣀⣀⣀⣙⣿⡛⠉⠁⠀⢀⣀⣀⣠⣤⣤⣤⠤⣭⣝⣿⣄\n⠀⠀⢠⡞⠁⠀⣾⠀⠀⠀⠀⠀⠀⣾⣛⣛⠋⠉⢀⣀⣀⡠⠤⢶⣶⢿⣿⣿⣤⡀⠀⠀⠈⡷⠒⠚⠉⠉⢠⣿⡿⢿⣿⣿⣦⡀⠀⠉⢻\n⠀⢀⡏⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠯⣉⠀⠀⠀⢠⣿⣿⣶⣿⠛⢻⣿⡆⠀⣰⠁⠀⠀⠀⠀⣿⣿⠿⣿⣏⣹⣿⣧⢀⣠⡞\n⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠦⢬⣙⠒⠤⢼⠿⢿⡿⠿⠿⠿⠛⠛⢉⡼⠛⠓⠒⠒⠶⠟⠛⠛⠛⠛⠛⠋⢩⡿⠛⠀\n⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠒⠒⠒⠒⠒⠒⣲⡾⠉⠉⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠋⠀⠀⠀\n⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠶⠋⠁⠀⠀⠀⠀⠈⠛⠢⢤⣤⠤⠤⠴⠒⢿⡁⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠙⢦⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣹⣄⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠋⠁⡀⠀⣀⡀⠀⠉⠉⠙⠓⠒⠲⠦⠤⠤⣤⣀⣀⣀⣀⣀⣀⣀⣀⣀⣤⠤⠶⠚⠉⢉⣿⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡅⠀⠀⠉⠉⠉⠉⠉⠓⠒⠶⠤⢤⣤⣀⣀⣀⣀⡀⠀⠀⠉⠉⠉⠉⠁⣀⣀⣀⣀⣠⣴⠟⠁⠀⠀\n⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀⠙⠒⠒⠒⠒⠒⠲⠦⠤⠤⣀⣀⣀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⢀⣿⠀⠀⠀⠀\n⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠦⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠙⠛⠛⠒⠒⠒⠒⠶⠶⠶⠶⢶⡦⠶⠒⠋⠁⠀⠀⠀⠀\n⠟⠿⢿⡶⢤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠔⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠉⠓⠦⣭⣉⠓⠒⠶⠦⠤⢤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⡤⠖⠚⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠈⠙⠓⠲⠦⢤⣤⣤⣀⣀⣀⣉⣉⣉⣉⣉⡉⢉⣉⣉⣉⣉⣩⣭⠟⠛⠷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠈⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'
+    print(greeting)
+    clientSocket.connect((host,port))
     print(f'\033[1;32;40mConnecting to {host}:{port}......')
-    i = 0
-    if i == 1:
-        dataFromServer = clientSocket.recv(512)
-    if i == 0:
-        dataFromServer = clientSocket.recv(512)
-        i+=1
+    dataFromServer = clientSocket.recv(1024)    
     while True:
+            
         print(dataFromServer.decode('utf-8'))
-        data = input('>command: ')
-        if data == 'ls':
+        data=input('> command: ')
+        if data=='ls':
             print('<============== LIST OF FILE ==============>')
-            clientSocket.send(data.encode('utf-8'))
+            clientSocket.send(data.encode('utf-8'))   
             listfile()
-        if data == 'd':
-            print('<============== DOWNLOAD ==============>')
-            clientSocket.send(data.encode('utf-8'))
+        elif data=='d':
+            print('<============== DOWNLOAD ==============>')   
+            clientSocket.send(data.encode('utf-8')) 
             dowload()
-        if data == 'u':
-            print('<============== UPLOAD ==============>')
-            clientSocket.send(data.encode('utf-8'))
+        elif data=='u':
+            print('<============== UPLOAD ==============>')   
+            clientSocket.send(data.encode('utf-8')) 
             upload()
-        elif data == 'q':
+        elif data=='q':
             clientSocket.send(data.encode('utf-8'))
             clientSocket.close()
             break
         else:
-            # data=input('command: ')
-            print('please try again')
-
+            #data=input('command: ')
+            print ('please try again')
+          
 except:
     print('error occur')
     clientSocket.close()
